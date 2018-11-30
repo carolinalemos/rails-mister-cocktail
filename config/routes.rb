e58d3f8c5698db119c1aa9422edd476107cc08a9
Carolina Lemos <carolinaperestrelodelemos@gmail.com>
2018-11-30 12:08:56 +0000

    added root to cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   resources :cocktails, only: [:new, :index, :show, :create] do
     resources :doses, only: [:new, :create, :destroy]
   end
